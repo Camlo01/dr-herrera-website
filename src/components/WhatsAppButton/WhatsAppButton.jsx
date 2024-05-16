cb175a1bada4928c3e1817e3dbffdb0d585a32d9
Camilo <camilobeltran174@gmail.com>
2024-05-16 09:35:01 -0500

    Improve WhatsApp Button logic

--- src/components/WhatsAppButton/WhatsAppButton.jsx
+++ src/components/WhatsAppButton/WhatsAppButton.jsx
@@ -4,10 +4,16 @@ import './WhatsAppButton.css'
 import WSICon from '/assets/icons/whatsapp-icon.png';
 
 function WhatsAppButton() {
+
+    const phoneNumber = `573155348437`
+    const hiMessage = `¡Buen día doc! Me gustaría conocer más acerca de... `
+
+    const completeURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${hiMessage}`
+
     return (
-        <a target='_blank' href="https://api.whatsapp.com/send?phone=573155348437&text=Buen%20día¡%20Estoy%20interesado%20en%20sus%20servicios.%20Doctor%20%7C%20Herrera%20%7C%20Cirugía%20%7C%20Bariátrica%20%7C%20Obesidad%20%7C%20Bogotá%20%7C%20Villavicencio%20" className="ws-btn">
+        <a target='_blank' href={completeURL} className="ws-btn" >
             <img src={WSICon} alt="WhatsApp icon" />
-        </a>
+        </a >
     );
 }
 
