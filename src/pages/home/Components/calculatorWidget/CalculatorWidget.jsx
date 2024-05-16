1c22bf8729e82d1c1cd3f777f508e16ab70f5ddc
Camilo <camilobeltran174@gmail.com>
2024-05-16 08:54:53 -0500

    Add calculator widget

---
+++ src/pages/home/Components/calculatorWidget/CalculatorWidget.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+import './calculatorWidget.css'
+
+import IMCIcon from '/assets/icons/IMC-icon.png'
+
+function CalculatorWidget() {
+
+    const [isClosed, setIsClosed] = useState(false);
+
+    const handleClose = () => {
+        console.log('Click handleClose')
+        setIsClosed(true);
+    };
+
+    const handleMouseEnter = () => {
+        console.log('Click handleMouseEnter')
+        setIsClosed(false);
+    };
+
+    const handleTakeTest = () => {
+        setIsClosed(true);
+    }
+
+    return (
+
+        <div className={`calculator-widget ${isClosed ? 'closed' : ''}`}
+            onMouseEnter={handleMouseEnter}
+        >
+            <button type='button' onClick={handleClose}>X</button>
+            <img src={IMCIcon} alt="Indicator icon" />
+            <h2>Calcula tu IMC</h2>
+            <p>Determina con ayuda de nuestra calculadora si estás dentro de tu peso ideal (índice de masa corporal)</p>
+            <h3 onClick={handleTakeTest}>Realizar Test</h3>
+        </div>
+
+    );
+}
+
+export default CalculatorWidget;
