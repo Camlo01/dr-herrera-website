c910732fd686dc25d0d9dad890c7e351f33532b0
Camilo <camilobeltran174@gmail.com>
2024-05-24 15:12:32 -0500

    Add ImcCalculatorSection with its styles

---
+++ src/pages/home/Sections/imcCalculatorSection/ImcCalculatorSection.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+import CalculatorWidget from '../../components/calculatorWidget/CalculatorWidget.jsx'
+import ImcCalculator from '../../components/ImcCalculator/ImcCalculator.jsx'
+import ImcResult from '../../components/imcResult/ImcResult.jsx';
+
+import '../imcCalculatorSection/imcCalculatorSection.css'
+
+function ImcCalculatorSection() {
+
+    const [imcResult, setImcResult] = useState(0)
+
+    return (
+        <div className='imcCalculator-section'>
+
+            <div className='body'>
+                <h2>Calculadora IMC</h2>
+                <h3>¿Qué es ?</h3>
+                <p>Una calculadora de índice de masa corporal (IMC) es una herramienta que utiliza tus medidas de peso y altura para calcular un número que indica tu nivel relativo de grasa corporal.</p>
+            </div>
+
+
+
+            <div className='cards-container'
+                style={{ display: 'flex', height: "fit-content" }}>
+                <CalculatorWidget />
+                <ImcCalculator setImcResult={setImcResult} />
+                <ImcResult imcResult={imcResult} />
+            </div>
+        </div>
+    );
+}
+
+export default ImcCalculatorSection;
