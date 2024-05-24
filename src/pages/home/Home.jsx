8207b202fdf275588cba98c1deb5b2386657747a
Camilo <camilobeltran174@gmail.com>
2024-05-24 15:12:04 -0500

    Add two sections Introduction and ImcCalculator

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 
+import IntroductionSection from "./sections/introductionSection/IntroductionSection.jsx";
+import ImcCalculatorSection from './sections/imcCalculatorSection/ImcCalculatorSection.jsx'
+
+/**
+ * Page content
+ * 
+ * 1. Introduction
+ * 2. SurgerySection
+ * 3. CandidateSection
+ * 4. TestimonySection
+ * 5. SocialSection 
+ * 
+ * @returns Component 
+ */
 function Home() {
 
     return (
         <>
-            <h1>Inicio</h1>
+            <div>
+                <IntroductionSection />
+                <ImcCalculatorSection />
+            </div>
         </>
     );
 }
