21f8dcdae1d51184fa408332ed6e795d971458bc
Camilo <camilobeltran174@gmail.com>
2024-05-26 22:03:23 -0500

    Add Loading Component

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,8 +1,23 @@
 function Loading() {
     return (
-        <>
-            <h1>Loading</h1>
-        </>
+        <div
+            style={{
+                height: '100vh',
+                background: "rgb(24, 77, 152)",
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}
+        >
+            <h1 style={{
+                color: "white",
+                fontFamily: 'Nunito',
+                fontSize: '3.8rem',
+                height: 'fit-content',
+                width: 'fit-content',
+                margin: 0
+            }}>Cargando...</h1>
+        </div>
     );
 }
 
