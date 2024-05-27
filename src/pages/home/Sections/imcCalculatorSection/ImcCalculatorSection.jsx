2ee435dec6eb5c13724f44dbfa4f1860831b7775
Camilo <camilobeltran174@gmail.com>
2024-05-27 10:10:25 -0500

    Add id to section

--- src/pages/home/Sections/imcCalculatorSection/ImcCalculatorSection.jsx
+++ src/pages/home/Sections/imcCalculatorSection/ImcCalculatorSection.jsx
@@ -11,15 +11,13 @@ function ImcCalculatorSection() {
     const [imcResult, setImcResult] = useState(0)
 
     return (
-        <div className='imcCalculator-section'>
+        <div className='imcCalculator-section' id='calculadora-imc'>
 
             <div className='body'>
                 <h2>Calculadora IMC</h2>
                 <h3>¿Qué es ?</h3>
                 <p>Una calculadora de índice de masa corporal (IMC) es una herramienta que utiliza tus medidas de peso y altura para calcular un número que indica tu nivel relativo de grasa corporal.</p>
             </div>
-
-
 
             <div className='cards-container'
                 style={{ display: 'flex', height: "fit-content" }}>
