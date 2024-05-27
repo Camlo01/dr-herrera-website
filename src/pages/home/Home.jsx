4247144b6a31e368221acc35aa3107a4588ecbb3
Camilo <camilobeltran174@gmail.com>
2024-05-26 22:05:59 -0500

    Add all sections of Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,27 +1,21 @@
 import React from "react";
 
+// Sections
 import IntroductionSection from "./sections/introductionSection/IntroductionSection.jsx";
 import ImcCalculatorSection from './sections/imcCalculatorSection/ImcCalculatorSection.jsx'
+import SurgerySection from './sections/surgerySection/SurgerySection.jsx'
+import TestimonySection from "./sections/testimonySection/TestimonySection.jsx";
+import SocialSection from "./sections/socialSection/SocialSection.jsx";
 
-/**
- * Page content
- * 
- * 1. Introduction
- * 2. SurgerySection
- * 3. CandidateSection
- * 4. TestimonySection
- * 5. SocialSection 
- * 
- * @returns Component 
- */
 function Home() {
 
     return (
         <>
-            <div>
-                <IntroductionSection />
-                <ImcCalculatorSection />
-            </div>
+            <IntroductionSection />
+            <SurgerySection />
+            <ImcCalculatorSection />
+            <TestimonySection />
+            <SocialSection />
         </>
     );
 }
