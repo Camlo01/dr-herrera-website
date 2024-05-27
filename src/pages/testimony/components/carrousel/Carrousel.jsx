538bba71127b48fd9ab69f785315b8540545e4d4
Camilo <camilobeltran174@gmail.com>
2024-05-27 12:23:33 -0500

    Add content to carrousel and fix styles

--- src/pages/testimony/components/carrousel/Carrousel.jsx
+++ src/pages/testimony/components/carrousel/Carrousel.jsx
@@ -8,41 +8,74 @@ import './carrousel.css'
 function Carrousel() {
     const settings = {
         className: "",
+        lazyLoad: true,
         dots: true,
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         adaptiveHeight: true,
-        
-        autoplay: true,  
-        autoplaySpeed: 3000
     };
-
     return (
         <div className="slider-container">
             <Slider {...settings}>
+
+                {/* First Case  */}
                 <div className="card">
-                    <img src="" alt="Photography" />
+                    <div className="content-container">
+                        <video height="451px" controls>
+                            <source src={`./assets/videos/testimony/Testimonio-video-1.MP4`} type="video/mp4" />
+                            Tu navegador no soporta el elemento de video.
+                        </video>
+                    </div>
                     <div>
-                        <h3> <b>Peso Perdido:</b> -20 KG </h3>
+                        <h3> <b>Peso Perdido:</b> -48 KG </h3>
                         <hr />
-                        <p>"La cirugía bariátrica cambió mi vida por completo. Después de años de luchar con mi peso, finalmente he encontrado una solución que funciona. Me siento más saludable, más feliz y más segura de mí misma. <b> ¡Gracias al equipo de cirugía bariátrica por su increíble apoyo!"</b></p>
+                        <p>En esta oportunidad esta paciente nos cuenta su experiencia después de un año...</p>
                     </div>
                 </div>
+
+                {/* Second Case  */}
                 <div className="card">
-                    <img src="" alt="Photography" />
+                    <div className="content-container">
+                        <video height="451px" controls>
+                            <source src={`./assets/videos/testimony/Testimonio-video-2.MP4`} type="video/mp4" />
+                            Tu navegador no soporta el elemento de video.
+                        </video>
+                    </div>
                     <div>
-                        <h3> <b>Peso Perdido:</b> -20 KG </h3>
+                        <h3> <b>Peso Perdido:</b> -30 KG </h3>
                         <hr />
-                        <p>"La cirugía bariátrica cambió mi vida por completo. Después de años de luchar con mi peso, finalmente he encontrado una solución que funciona. Me siento más saludable, más feliz y más segura de mí misma. <b> ¡Gracias al equipo de cirugía bariátrica por su increíble apoyo!"</b></p>
+                        <p>Juan David después de 9 meses nos cuenta que inicio su procedo debido a...</p>
                     </div>
                 </div>
+
+                {/* Third Case  */}
                 <div className="card">
-                    <img src="" alt="Photography" />
+                    <div className="content-container">
+                        <video height="451px" controls>
+                            <source src={`./assets/videos/testimony/Testimonio-video-3.MP4`} type="video/mp4" />
+                            Tu navegador no soporta el elemento de video.
+                        </video>
+                    </div>
                     <div>
-                        <h3> <b>Peso Perdido:</b> -20 KG </h3>
+                        <h3> <b>Peso Perdido:</b> -33 KG </h3>
                         <hr />
-                        <p>"La cirugía bariátrica cambió mi vida por completo. Después de años de luchar con mi peso, finalmente he encontrado una solución que funciona. Me siento más saludable, más feliz y más segura de mí misma. <b> ¡Gracias al equipo de cirugía bariátrica por su increíble apoyo!"</b></p>
+                        <p>Conoce el caso de nuestro paciente que pesando 127 kilos, apenas 3 meses después de su cirugía logró bajar 33 kilos...</p>
+                    </div>
+                </div>
+
+                {/* Fourth Case  */}
+                <div className="card">
+                    <div className="content-container">
+                        <video height="451px" controls>
+                            <source src={`./assets/videos/testimony/Testimonio-video-4.MP4`} type="video/mp4" />
+                            Tu navegador no soporta el elemento de video.
+                        </video>
+                    </div>
+                    <div>
+                        <h3> <b>Peso Perdido:</b> -30 KG </h3>
+                        <hr />
+                        <p>Conoce la historia de nuestro paciente Juan David que en segundo slide nos cuenta cómo ha sido su experiencia y el porqué decidió operarse</p>
                     </div>
                 </div>
             </Slider>
