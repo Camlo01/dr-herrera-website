3ffc84246a1d14493503842907e8388a27363643
Camilo <camilobeltran174@gmail.com>
2024-05-26 22:02:50 -0500

    Add behavior for smooth scroll to sections

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
-import { Suspense } from 'react'
-import { Outlet } from "react-router-dom";
+import { Suspense, useEffect } from 'react'
+import { Outlet, useLocation } from "react-router-dom";
 
 import NavegationBar from '../NavegationBar/NavegationBar'
 import Loading from '../Loading/Loading';
@@ -7,6 +7,19 @@ import Footer from '../Footer/Footer';
 import WhatsAppButton from '../WhatsAppButton/WhatsAppButton';
 
 export default function Layout() {
+
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.hash) {
+            const element = document.querySelector(location.hash);
+            if (element) {
+                const yOffset = -35; // Ajuste del margen superior de 100px
+                const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+                window.scrollTo({ top: y, behavior: 'smooth' });
+            }
+        }
+    }, [location]);
 
     return (
         <>
