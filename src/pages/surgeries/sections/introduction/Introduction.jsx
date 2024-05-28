79845e02c877818a2dd2ea796224ccbb1f4b6d99
Camilo <camilobeltran174@gmail.com>
2024-05-28 09:28:56 -0500

    Add introduction section of surgeries page

---
+++ src/pages/surgeries/sections/introduction/Introduction.jsx
@@ -0,0 +1,15 @@
+
+import './introduction.css'
+
+function IntroductionSection() {
+    return (
+        <section className="introduction-section">
+            <h1>Cirugías</h1>
+            <h2>Bienvenido a nuestra sección de cirugías bariátricas.</h2>
+            <p>La cirugía bariátrica contempla un gran número de cirugías aprobadas, dentro de estas las más comúnmente conocidas son: Manga (sleeve) gástrica y Bypass gástrico. Ante el avance tecnológico y estudios sobre la fisiopatología del paciente obeso y sus comorbilidades, hay otras menos conocidas como: BAGUA (bypass gástrico de única anastomosis – mini bypass), SADI–S (Anastomosis única duodeno–ileal más gastrectomía vertical). Y una realizada en casos de reganacia de peso, conocida como cirugía de revisión.  </p>
+            <p>A través de estas página, conocerás las diferentes cirugías que tenemos.</p>
+        </section>
+    );
+}
+
+export default IntroductionSection;
