269e3bdbf3fe980d14f585bcf7d61dd5f20e7a3e
Camilo <camilobeltran174@gmail.com>
2024-05-28 22:17:33 -0500

    Improve styles add a background image

--- src/pages/testimony/sections/invitation/Invitation.jsx
+++ src/pages/testimony/sections/invitation/Invitation.jsx
@@ -7,7 +7,10 @@ function Invitation() {
     return (
         <div className='testimony-invitation'>
             <p>Únete a nuestra comunidad en línea y descubre cómo la cirugía bariátrica puede marcar la diferencia en tu vida y en la de otros. </p>
-            <p>¡Te esperamos en nuestras redes sociales para seguir inspirándonos mutuamente en  <b> nuestro viaje hacia una vida más saludable y plena!</b></p>
+
+            <div className='divider'>
+                <p>¡Te esperamos en nuestras redes sociales para seguir inspirándonos mutuamente en  <b> nuestro viaje hacia una vida más saludable y plena!</b></p>
+            </div>
 
             <Link to={'/Contacto'}> <button>Contactar</button> </Link>
         </div>
