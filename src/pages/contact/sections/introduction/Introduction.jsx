fbf930c11cc7b21d9b60a5a880313ce73e6c59b8
Camilo <camilobeltran174@gmail.com>
2024-06-03 23:35:27 -0500

    Remove title

--- src/pages/contact/sections/introduction/Introduction.jsx
+++ src/pages/contact/sections/introduction/Introduction.jsx
@@ -4,8 +4,6 @@ import './introduction.css'
 function IntroductionSection() {
     return (
         <section className="introduction-section">
-            <h1>Contacto</h1>
-
             <p> <b>¡Bienvenido a nuestra sección de contacto!</b> Aquí encontrarás todas las formas de comunicarte con nosotros de manera fácil y rápida.</p>
             <p>Encuentra nuestra ubicación en el mapa interactivo para ver donde estamos ubicados.</p>
         </section >
--- src/pages/surgeries/sections/introduction/Introduction.jsx
+++ src/pages/surgeries/sections/introduction/Introduction.jsx
@@ -4,10 +4,21 @@ import './introduction.css'
 function IntroductionSection() {
     return (
         <section className="introduction-section">
-            <h1>Cirugías</h1>
-            <h2>Bienvenido a nuestra sección de cirugías bariátricas.</h2>
             <p>La cirugía bariátrica contempla un gran número de cirugías aprobadas, dentro de estas las más comúnmente conocidas son: Manga (sleeve) gástrica y Bypass gástrico. Ante el avance tecnológico y estudios sobre la fisiopatología del paciente obeso y sus comorbilidades, hay otras menos conocidas como: BAGUA (bypass gástrico de única anastomosis – mini bypass), SADI–S (Anastomosis única duodeno–ileal más gastrectomía vertical). Y una realizada en casos de reganacia de peso, conocida como cirugía de revisión.  </p>
             <p>A través de estas página, conocerás las diferentes cirugías que tenemos.</p>
+
+            <div className='content-section'>
+                <p>Sección de Cirugías Bariatricas:</p>
+                <ul>
+                    <li>¿Qué es la cirugía bariátrica?</li>
+                    <li>¿Porqué se realiza?</li>
+                    <li>Manga Gástrica </li>
+                    <li>Bypass Gástrico </li>
+                    <li>BAGUA (bypass gástrico de única <br /> anastomosis – mini bypass) </li>
+                    <li>Revisión Bariátrica </li>
+                    <li>Cirugía Metabólica </li>
+                </ul>
+            </div>
         </section>
     );
 }
