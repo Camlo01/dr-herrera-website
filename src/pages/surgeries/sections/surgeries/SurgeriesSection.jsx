5cc0d2dc1731184e92c0b5c3c9beba03a9ae4b92
Camilo <camilobeltran174@gmail.com>
2024-06-03 23:44:13 -0500

    Improve content | Add images to manga gatrica section

--- src/pages/surgeries/sections/surgeries/SurgeriesSection.jsx
+++ src/pages/surgeries/sections/surgeries/SurgeriesSection.jsx
@@ -6,9 +6,8 @@ import firstImage1 from '/assets/images/surgeries/image-surgery-1.png'
 import firstImage2 from '/assets/images/surgeries/image-surgery-2.png'
 import firstImage3 from '/assets/images/surgeries/image-surgery-3.png'
 
-import gastricSleeveImage1 from '/assets/images/surgeries/gastric-sleeve-image-1.png'
-import gastricSleeveImage2 from '/assets/images/surgeries/gastric-sleeve-image-2.png'
-import gastricSleeveImage3 from '/assets/images/surgeries/gastric-sleeve-image-3.png'
+import imgGastricSleeve1 from '/assets/images/surgeries/gastric-sleeve-pt1.jpg'
+import imgGastricSleeve2 from '/assets/images/surgeries/gastric-sleeve-pt2.jpg'
 
 function SurgeriesSection() {
     return (
@@ -22,23 +21,10 @@ function SurgeriesSection() {
                 <img src={firstImage1} alt="Dr. Wilmar Herrera" className='first-image' />
             </picture>
 
-            <div className='content-section'>
-                <p>Sección de Cirugías Bariatricas :</p>
-                <ul>
-                    <li>¿Qué es la cirugía bariátrica?</li>
-                    <li>¿Porqué se realiza?</li>
-                    <li>Manga Gástrica </li>
-                    <li>Bypass Gástrico </li>
-                    <li>BAGUA (bypass gástrico de única <br /> anastomosis – mini bypass) </li>
-                    <li>Revisión Bariátrica </li>
-                    <li>Cirugía Metabólica </li>
-                </ul>
-            </div>
-
             <div className='what-is-section surgery-section'>
                 <h2>¿Qué es la cirugía bariátrica?</h2>
                 <p>Según ACOCIB (asociacion colombiana de obesidad y cirugia bariatrica), la cirugia bariatrica es: </p>
-                <q>“La palabra Bariátrica procede del griego BAROS que significa peso y IATRICS que significa tratamiento. La Cirugía Bariátrica se define como el conjunto de procedimientos quirúrgicos diseñados con la intención de perder peso y mejorar las enfermedades asociadas. Dichos procedimientos quirúrgicos se han enfocado en modificar anatómicamente el tracto digestivo para producir restricción a nivel del estómago y disminución de la absorción a nivel del intestino delgado, lo que conlleva a cambios entero-hormonales con afección en diferentes órganos blancos y en el hipotálamo.” </q>
+                <p>La palabra Bariátrica procede del griego BAROS que significa peso y IATRICS que significa tratamiento. La Cirugía Bariátrica se define como el conjunto de procedimientos quirúrgicos diseñados con la intención de perder peso y mejorar las enfermedades asociadas. Dichos procedimientos quirúrgicos se han enfocado en modificar anatómicamente el tracto digestivo para producir restricción a nivel del estómago y disminución de la absorción a nivel del intestino delgado, lo que conlleva a cambios entero-hormonales con afección en diferentes órganos blancos y en el hipotálamo.</p>
                 <p>Yo defino la  cirugía  bariátrica como  un conjunto de procedimientos quirúrgicos que se realizan a personas con obesidad, dicha intervención se convierte en una gran herramienta efectiva y segura a corto y largo plazo, siempre y cuando exista un compromiso propio del paciente y un acompañamiento integral del equipo multidisciplinario para  generar un balance de hábitos saludables (dieta y actividad física), todo lo anterior con la finalidad de lograr y permanecer con un peso ideal y sin alteraciones nutricionales por el resto de la vida. </p>
             </div>
 
@@ -50,31 +36,32 @@ function SurgeriesSection() {
                 <h3>¿Porqué se realiza?</h3>
                 <p>Para ayudarte a perder el exceso de peso y reducir el riesgo de que se presenten problemas de salud relacionados con el peso que podrían poner en riesgo tu vida, como los siguientes:</p>
                 <ul>
-                    <li>Enfermedad por reflujo gastroesofágico</li>
-                    <li>Cardiopatía</li>
-                    <li>Presión arterial alta</li>
-                    <li>Colesterol alto</li>
-                    <li>Apnea obstructiva del sueño</li>
-                    <li>Diabetes tipo 2</li>
                     <li>Accidente cerebrovascular</li>
+                    <li>Apnea obstructiva del sueño</li>
+                    <li>Cardiopatía</li>
                     <li>Cáncer</li>
+                    <li>Colesterol alto</li>
+                    <li>Diabetes tipo 2</li>
+                    <li>Enfermedad por reflujo gastroesofágico</li>
+                    <li>Enfermedades osteoarticulares</li>
                     <li>Infertilidad</li>
+                    <li>Presión arterial alta</li>
+                    <li>Entre otros</li>
                 </ul>
             </div>
 
             <div className='manga-gastrica-section surgery-section'>
                 <h3>Manga Gástrica</h3>
-                <picture>
-                    <source srcSet={gastricSleeveImage3} media="(min-width: 1280px)" />
-                    <source srcSet={gastricSleeveImage2} media="(min-width: 744px) and (max-width: 1279px)" />
-                    <source srcSet={gastricSleeveImage1} media="(max-width: 743px)" />
-                    <img src={gastricSleeveImage1} alt="Dr. Wilmar Herrera" className='' />
-                </picture>
+                <div className='container-photos'>
+                    <img src={imgGastricSleeve1} alt="Manga gástrica" />
+                    <img src={imgGastricSleeve2} alt="Doc herrera sonteniendo una manga gástrica" />
+                </div>
+
                 <p>La manga gástrica,  o gastrectomía vertical, es la cirugia bariátrica mas realizada a nivel mundial. Se trata de extirpar entre un 70-80% de manera vertical el estómago, reduciendo el tamaño del estómago, limitando la cantidad de alimentos consumidos y disminuyendo la producción de la hormona del hambre, la Grelina. Este método, técnicamente menos demandante que el bypass gástrico, promueve una pérdida de peso significativa y puede mejorar las condiciones médicas asociadas con la obesidad. </p>
 
                 <div className='video-container'>
                     <video controls >
-                        <source src={`/assets/videos/surgeries/Manga-gastrica.mov`} type="video/mp4" />
+                        <source src={`/assets/videos/surgeries/Manga-gastrica.mp4`} type="video/mp4" />
                         Tu navegador no soporta el elemento de video.
                     </video>
                     <h4>Manga Gástrica</h4>
@@ -82,7 +69,7 @@ function SurgeriesSection() {
 
                 <div className='indications'>
                     <h4>Indicaciones</h4>
-                    <p>Cual grado de obesidad, ideal en obesidad Grado I (IMC 30-34.9) y II (IMC 35-39.9)</p>
+                    <p>Cualquier grado de obesidad, ideal en obesidad Grado I (IMC 30-34.9) y II (IMC 35-39.9)</p>
                 </div>
             </div>
 
@@ -94,7 +81,7 @@ function SurgeriesSection() {
                 <div className='videos-container'>
                     <div className='video-container'>
                         <video controls >
-                            <source src={`/assets/videos/surgeries/Bypass-grastrico.mov`} type="video/mp4" />
+                            <source src={`/assets/videos/surgeries/Bypass-grastrico.mp4`} type="video/mp4" />
                             Tu navegador no soporta el elemento de video.
                         </video>
                         <h4>Bypass Gástrico</h4>
@@ -102,7 +89,7 @@ function SurgeriesSection() {
 
                     <div className='video-container'>
                         <video controls >
-                            <source src={`/assets/videos/surgeries/Mini-bypass-gastrico.mov`} type="video/mp4" />
+                            <source src={`/assets/videos/surgeries/Mini-bypass-gastrico.mp4`} type="video/mp4" />
                             Tu navegador no soporta el elemento de video.
                         </video>
                         <h4>Mini Bypass Gástrico</h4>
@@ -111,7 +98,7 @@ function SurgeriesSection() {
 
                 <div className='indications'>
                     <h4>Indicaciones</h4>
-                    <p>{`Obesidad con IMC >40, o >35 con comorbilidades asociadas como Diabetes Mellitus, hipertensión arterial o apnea del sueño.  En algunos casos, puedes reunir los requisitos de ciertos tipos de cirugía de pérdida de peso si tu índice de masa corporal es de 30 a 34 y tienes problemas de salud graves relacionados con el peso.`}</p>
+                    <p>{`Obesidad con IMC > 40, o > 35 con comorbilidades asociadas como Diabetes Mellitus, hipertensión arterial o apnea del sueño.  En algunos casos, puedes reunir los requisitos de ciertos tipos de cirugía de pérdida de peso si tu índice de masa corporal es de 30 a 34 y tienes problemas de salud graves relacionados con el peso.`}</p>
                 </div>
             </div>
 
