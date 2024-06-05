ad328082dc4c314ecbb5be115f90824f3c9f377c
Camilo <camilobeltran174@gmail.com>
2024-06-05 13:26:35 -0500

    Add phrase to divider section

--- src/pages/testimony/sections/invitation/Invitation.jsx
+++ src/pages/testimony/sections/invitation/Invitation.jsx
@@ -9,7 +9,7 @@ function Invitation() {
             <p>Únete a nuestra comunidad en línea y descubre cómo la cirugía bariátrica puede marcar la diferencia en tu vida y en la de otros. </p>
 
             <div className='divider'>
-                <p>¡Te esperamos en nuestras redes sociales para seguir inspirándonos mutuamente en  <b> nuestro viaje hacia una vida más saludable y plena!</b></p>
+                <p>Te esperamos en nuestras redes sociales. <b>Tienes una vida ¡Ahora juntos la haremos mejor!</b></p>
             </div>
 
             <Link to={'/Contacto'}> <button>Contactar</button> </Link>
