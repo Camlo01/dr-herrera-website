58e4541938f883d4f09d3c33ef141bad193cf085
Camilo <camilobeltran174@gmail.com>
2024-06-09 20:49:09 -0500

    Improved behavior of Testimony Section in Home Page

--- src/pages/home/Sections/testimonySection/TestimonySection.jsx
+++ src/pages/home/Sections/testimonySection/TestimonySection.jsx
@@ -1,8 +1,7 @@
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import './testimonySection.css'
-
-// testimony images
 
 import testimonyImage11 from '/assets/images/testimony/testimonio-1-1.jpeg'
 import testimonyImage12 from '/assets/images/testimony/testimonio-1-2.jpeg'
@@ -11,9 +10,50 @@ import testimonyImage22 from '/assets/images/testimony/testimonio-2-2.jpeg'
 import testimonyImage31 from '/assets/images/testimony/testimonio-3-1.jpeg'
 import testimonyImage32 from '/assets/images/testimony/testimonio-3-2.jpeg'
 
-
-
 function TestimonySection() {
+
+    // Initial values
+    const [isWithinWidthRange, setIsWithinWidthRange] = useState(false);
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    // Arrays of images
+    const images1 = [testimonyImage11, testimonyImage21, testimonyImage31];
+    const images2 = [testimonyImage12, testimonyImage22, testimonyImage32];
+
+    // Set behavior for when 
+    useEffect(() => {
+        const handleResize = () => {
+            const screenWidth = window.innerWidth;
+            setIsWithinWidthRange(screenWidth >= 744 && screenWidth <= 1280);
+
+            if (!isWithinWidthRange && currentIndex !== 0) {
+                setCurrentIndex(0);
+            }
+        };
+
+        handleResize();
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [isWithinWidthRange, currentIndex]);
+
+    useEffect(() => {
+        let intervalId;
+
+        if (isWithinWidthRange) {
+            intervalId = setInterval(() => {
+                setCurrentIndex(prevIndex => (prevIndex === 0 ? 1 : (prevIndex === 1 ? 2 : 0)));
+            }, 2700);
+        } else {
+            clearInterval(intervalId);
+        }
+
+        return () => clearInterval(intervalId);
+    }, [isWithinWidthRange]);
+
     return (
         <section className="testimony-section" id='testimonios'>
             <h2>Testimonios</h2>
@@ -26,13 +66,13 @@ function TestimonySection() {
 
                         <div className='card'>
                             <div className='before'>
-                                <img src={testimonyImage11} alt="Paciente antes de la cirugía bariátrica" />
+                                <img src={images1[currentIndex]} alt="Paciente antes de la cirugía bariátrica" />
                                 <div className='text'>
                                     <p>Antes</p>
                                 </div>
                             </div>
                             <div className='after'>
-                                <img src={testimonyImage12} alt="Paciente después de la cirugía bariátrica" />
+                                <img src={images2[currentIndex]} alt="Paciente después de la cirugía bariátrica" />
                                 <div className='text'>
                                     <p>Después</p>
                                 </div>
@@ -77,10 +117,7 @@ function TestimonySection() {
                                 <Link to={'/Testimonios'}>Ver Completo</Link>
                             </div>
                         </div>
-
-
                     </div>
-
                 </div>
                 <div className='stories'>
                     <h3>Tu Historia Puede Ser la Próxima</h3>
